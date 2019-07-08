14df1a58ed889eb307b7a9d45f303cd3e412682d
Hiroyuki Takakura <takakura@codeninth.com>
2019-07-08 18:14:58 +0900

    add free flags

--- src/Constants/SOUND_NAME.js
+++ src/Constants/SOUND_NAME.js
@@ -4,47 +4,47 @@ const SOUND_NAME = new Map([
     110**
     - ピアノ系
   */
-    ["piano",{ id: 11001, tag: ["piano"] }],
-    ["bright_piano", { id: 11002, tag: ["piano"] }],
-    ["bell_piano", { id: 11003, tag: ["piano"] }],
-    ["clavi", { id: 11004, tag: ["piano"] }],
+    ["piano",{ id: 11001, tag: ["piano"], free: true }],
+    ["bright_piano", { id: 11002, tag: ["piano"], free: false }],
+    ["bell_piano", { id: 11003, tag: ["piano"], free: false }],
+    ["clavi", { id: 11004, tag: ["piano"], free: false }],
 
     /*
     111**
     - キーボード・オルガン系
   */
-    ["keyboard_01", { id: 11101, tag: ["keyboard"] }],
-    ["keyboard_02", { id: 11102, tag: ["keyboard"] }],
-    ["organ", { id: 11103, tag: ["keyboard"] }],
-    ["church_organ", { id: 11104, tag: ["keyboard"] }],
+    ["keyboard_01", { id: 11101, tag: ["keyboard"], free: true }],
+    ["keyboard_02", { id: 11102, tag: ["keyboard"], free: true }],
+    ["organ", { id: 11103, tag: ["keyboard"], free: false }],
+    ["church_organ", { id: 11104, tag: ["keyboard"], free: false }],
 
     /*
     112**
     - ベース系
   */
-    ["minimal_synth_bass_01", { id:  11205, tag: ["bass"] }],
+    ["minimal_synth_bass_01", { id:  11205, tag: ["bass"], free: true }],
 
     /*
     113**
     - ギター系
   */
-    ["n_guitar", { id: 11301, tag: ["guitar"] }],
-    ["e_guitar_01", { id: 11302, tag: ["guitar"] }],
-    ["e_guitar_02", { id: 11303, tag: ["guitar"] }],
+    ["n_guitar", { id: 11301, tag: ["guitar"], free: true }],
+    ["e_guitar_01", { id: 11302, tag: ["guitar"], free: false }],
+    ["e_guitar_02", { id: 11303, tag: ["guitar"], free: false }],
 
     /*
     114**
     - パッド系
   */
-    ["string_pad", { id: 11401, tag: ["soft"] }],
-    ["voice_pad", { id: 11402, tag: ["soft"] }],
+    ["string_pad", { id: 11401, tag: ["soft"], free: true }],
+    ["voice_pad", { id: 11402, tag: ["soft"], free: false }],
 
     /*
     115**
     - リード系
   */
-    ["square_lead", { id: 11501, tag: ["solid"] }],
-    ["solid_lead", { id: 11502, tag: ["solid"] }],
+    ["square_lead", { id: 11501, tag: ["solid"], free: true }],
+    ["solid_lead", { id: 11502, tag: ["solid"], free: false }],
 
     /*
     116**
@@ -55,22 +55,22 @@ const SOUND_NAME = new Map([
     117**
     - ノンジャンル
   */
-    ["steel_pan", { id: 11701, tag: ["ethnic"] }],
+    ["steel_pan", { id: 11701, tag: ["ethnic"], free: false }],
 
     /*
     21***
     - ドラム系
   */
-    ["basic_drums", { id: 21001, tag: ["real_drums"] }],
-    ["standard_drums", { id: 21002, tag: ["real_drums"] }],
-    ["hiphop_drums", { id: 21003, tag: ["big_drums"] }],
-    ["trip_drums", { id: 21004, tag: ["big_drums"] }],
-    ["room_drums", { id: 21005, tag: ["big_drums"] }],
+    ["basic_drums", { id: 21001, tag: ["real_drums"], free: true }],
+    ["standard_drums", { id: 21002, tag: ["real_drums"], free: false }],
+    ["hiphop_drums", { id: 21003, tag: ["big_drums"], free: false }],
+    ["trip_drums", { id: 21004, tag: ["big_drums"], free: false }],
+    ["room_drums", { id: 21005, tag: ["big_drums"], free: false }],
 
-    ["minimal_drums", { id: 21101, tag: ["small_drums"] }],
-    ["simple_drums", { id: 21102, tag: ["small_drums"] }],
-    ["dance_drums", { id: 21103, tag: ["small_drums"] }],
-    ["techno_drums", { id: 21104, tag: ["small_drums"] }]
+    ["minimal_drums", { id: 21101, tag: ["small_drums"], free: true }],
+    ["simple_drums", { id: 21102, tag: ["small_drums"], free: false }],
+    ["dance_drums", { id: 21103, tag: ["small_drums"], free: false }],
+    ["techno_drums", { id: 21104, tag: ["small_drums"], free: false }]
 
 ])
 
