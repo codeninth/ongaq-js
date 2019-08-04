1ff37180b24c59953bbfb06ce42178f1c462b499
HiroyukiTakakura <takakura@codeninth.com>
2019-08-04 23:39:12 +0900

    update drum note names

--- src/Constants/DRUM_NOTE.js
+++ src/Constants/DRUM_NOTE.js
@@ -5,11 +5,12 @@ const DRUM_NOTE = new Map([
     ["hihat2","2$2"],
     ["snare","1$3"],
     ["snare2","2$3"],
-    ["side","1$4"],
-    ["side2","2$4"],
+    ["tom","1$4"],
+    ["tom2","2$4"],
+    ["side","1$5"],
+    ["side2","2$5"],
     ["cymbal","1$12"],
     ["cymbal2","2$12"]
- 
 ])
 
 export default DRUM_NOTE
