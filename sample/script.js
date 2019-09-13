e688a3fc0e8697f4636ab298cf6f0195034b74f7
Hiroyuki Takakura <takakura@codeninth.com>
2019-09-13 22:28:03 +0900

    update sample

--- sample/script.js
+++ sample/script.js
@@ -1,21 +1,41 @@
-const bass_drum = new Part({
-    sound: "small_cube"
+const drumPart = new Part({
+    sound: "small_cube_drums",
+    measure: 4
 })
-bass_drum.add("note", {
-    key: ["kick"],
-    active: noteIndex => noteIndex % 8 === 0
-})
+drumPart.add(
+    new Filter({
+        key: ["kick"],
+        active: n => n % 8 === 0
+    })
+)
+drumPart.add(
+    new Filter({
+        key: ["hihat"],
+        active: n => n % 8 === 4
+    })
+)
+
 const loop = new Loop({
     id: "my-first-loop",
-    bpm: 120
+    bpm: 110
 })
-loop.add(bass_drum)
+loop.add(drumPart)
 
 const ongaq = new Ongaq({
-    api_key: "YOUR_API_KEY"
+    api_key: "master_api_key_00",
+    volume: 50
 })
-ongaq.import(loop)
-.then((l) => {
-    console.log(l)
-    // ongaq.play()
+
+ongaq.import(loop).then(() => {
+    console.log("ready to start.")
+    const b = document.getElementById("button")
+    b.onclick = () => {
+        if(ongaq.params.isPlaying){
+            ongaq.pause()
+            b.className = "start"
+        } else {
+            ongaq.start()
+            b.className = "pause"
+        }
+    }
 })
