2e3a786cca0e4e81ee031748e0e773688ecbdec6
Hiroyuki Takakura <takakura@codeninth.com>
2019-09-21 00:08:16 +0900

    update samples

---
+++ sample/script.example.js
@@ -0,0 +1,46 @@
+const my_drums = new Part({
+    sound: "small_cube_drums"
+})
+my_drums.add(
+    new Filter({
+        key: ["kick"],
+        active: n => n % 8 === 0
+    })
+)
+
+const my_guitar = new Part({
+    sound: "nylon_guitar",
+    measure: 4
+})
+
+my_guitar.add(new Filter({
+    key: new Chord("CM9"),
+    length: 16,
+    active: (n,m)=> n === 0 && m === 1
+}))
+
+const loop = new Loop({
+    id: "my-first-loop",
+    bpm: 110
+})
+loop.add(my_drums)
+loop.add(my_guitar)
+
+const ongaq = new Ongaq({
+    api_key: "master_api_key_00",
+    volume: 50
+})
+
+ongaq.import(loop).then(() => {
+    console.log("ready to start.")
+    const button = document.getElementById("button")
+    button.onclick = () => {
+        if(ongaq.params.isPlaying){
+            ongaq.pause()
+            button.className = "start"
+        } else {
+            ongaq.start()
+            button.className = "pause"
+        }
+    }
+})
--- sample/script.js
+++ sample/script.js
@@ -1,41 +1,4 @@
-const drumPart = new Part({
-    sound: "small_cube_drums",
-    measure: 4
-})
-drumPart.add(
-    new Filter({
-        key: ["kick"],
-        active: n => n % 8 === 0
-    })
-)
-drumPart.add(
-    new Filter({
-        key: ["hihat"],
-        active: n => n % 8 === 4
-    })
-)
-
-const loop = new Loop({
-    id: "my-first-loop",
-    bpm: 110
-})
-loop.add(drumPart)
-
-const ongaq = new Ongaq({
-    api_key: "master_api_key_00",
-    volume: 50
-})
-
-ongaq.import(loop).then(() => {
-    console.log("ready to start.")
-    const b = document.getElementById("button")
-    b.onclick = () => {
-        if(ongaq.params.isPlaying){
-            ongaq.pause()
-            b.className = "start"
-        } else {
-            ongaq.start()
-            b.className = "pause"
-        }
-    }
-})
+/*
+    - Write your own code here
+    - You can listen to a sample by removing this file and renaming script.example.js -> script.js
+*/
