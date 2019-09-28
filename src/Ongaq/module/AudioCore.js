f7ba1da5eb41d1dcce41e94a95b0a33cdf1b52ba
Hiroyuki Takakura <takakura@codeninth.com>
2019-09-28 21:29:50 +0900

    記述整理

--- src/Ongaq/module/AudioCore.js
+++ src/Ongaq/module/AudioCore.js
@@ -1,44 +1,38 @@
-const Module = (()=>{
+const context = new (window.AudioContext || window.webkitAudioContext)()
+const originTime = new Date().getTime()
+const powerMode = (()=>{
+    let u = window.navigator.userAgent
+    if (["iPhone", "iPad", "iPod","Android"].some( name=>u.indexOf(name) !== -1 )) return "low"
+    else return "middle"
+})()
 
-    const context = new (window.AudioContext || window.webkitAudioContext)()
-    const originTime = new Date().getTime()
-    const powerMode = (()=>{
-        let u = window.navigator.userAgent
-        if (["iPhone", "iPad", "iPod","Android"].some( name=>u.indexOf(name) !== -1 )) return "low"
-        else return "middle"
-    })()
+const AudioCore = {
 
-    class AudioCore {
+    context,
+    originTime,
+    powerMode,
+    toAudioBuffer: ({ src, length })=>{
 
-        get context(){ return context }
-        get originTime(){ return originTime }
-        get powerMode(){ return powerMode }
-
-        toAudioBuffer({ src, length }){
-
-            if(!src || !length) return false
-            return new Promise((resolve,reject)=>{
-                try {
-                    let buffer = new ArrayBuffer(length)
-                    let bufView = new Uint8Array(buffer)
-                    for (let i = 0; i < length; i++) bufView[i] = src.charCodeAt(i)
-                    context.decodeAudioData(
-                        buffer,
-                        buffer => buffer ? resolve(buffer) : reject(),
-                        reject
-                    )
-                } catch (err) {
-                    return reject(err)
-                }
-            })
-            
-
-        }
+        if(!src || !length) return false
+        return new Promise((resolve,reject)=>{
+            try {
+                let buffer = new ArrayBuffer(length)
+                let bufView = new Uint8Array(buffer)
+                for (let i = 0; i < length; i++) bufView[i] = src.charCodeAt(i)
+                context.decodeAudioData(
+                    buffer,
+                    buffer => buffer ? resolve(buffer) : reject(),
+                    reject
+                )
+            } catch (err) {
+                return reject(err)
+            }
+        })
 
     }
 
-    return AudioCore
+}
 
-}).call(undefined,window || {})
 
-export default new Module()
+
+export default AudioCore
