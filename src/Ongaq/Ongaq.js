7a919a832a46f4f29b764658f08b9cc0717caf08
Hiroyuki Takakura <takakura@codeninth.com>
2019-09-28 21:18:05 +0900

    記述整理

--- src/Ongaq/Ongaq.js
+++ src/Ongaq/Ongaq.js
@@ -2,7 +2,6 @@ import AudioCore from "./module/AudioCore"
 import Part from "./module/Part"
 import BufferYard from "./module/BufferYard"
 import Helper from "./module/Helper"
-// import manipulator from "./module/Manipulator"
 import DEFAULTS from "./module/defaults"
 
 const context = AudioCore.context
@@ -25,7 +24,7 @@ class Ongaq {
       this.previousVolume = this.volume
       this.bpm = bpm || DEFAULTS.BPM
       if (AudioCore.powerMode === "low") {
-        window.addEventListener("blur", () => { this.pauseScheduling() })
+        window.addEventListener("blur", () => { this.pauseScheduling() }) 
       }
       BufferYard.set({ api_key })
     }
@@ -39,7 +38,7 @@ class Ongaq {
           part.bpm = part.bpm || this.bpm
           part.notesInMeasure = part.notesInMeasure || DEFAULTS.NOTES_IN_MEASURE
           part.measure = part.measure || DEFAULTS.MEASURE
-          
+
           const p = new Part(part, {
             onLoad: () => {
               this.parts.set(p.id, p)
@@ -49,30 +48,7 @@ class Ongaq {
               reject(err)
             }
           })
-
         })
-    }
-
-    /*
-      @switch
-      - 次に再生するLoopを選択し、quotaを再設定する
-    */
-    // switch(o = {}){
-    //     return new Promise((resolve,reject)=>{
-    //         manipulator.switch(o) ? resolve() : reject()
-    //     })
-    // }
-
-    /*
-      @set bpm
-    */
-    set bpm(v){
-      if (typeof v !== "number" || v < 60 || v > 180) return false
-      this.parts.forEach(p=>{ p.bpm = v })
-      this._bpm = v
-    }
-    get bpm(){
-      return this._bpm
     }
 
     /*
@@ -133,7 +109,6 @@ class Ongaq {
       let cap = limitAge ? Math.max(...list) * limitAge : Infinity
       this.parts.forEach(p => { p.setQuota(cap) })
 
-      // this.loop.get(this.activeLoop).putTimerRight()
       this.scheduler = window.setInterval(this.routine, AudioCore.powerMode === "middle" ? 50 : 200)
     }
 
@@ -223,7 +198,7 @@ class Ongaq {
             isPlaying: this.isPlaying,
             originTime: AudioCore.originTime,
             currentTime: context.currentTime,
-            volume: this.volume * 100
+            volume: this.volume
         }
     }
 
@@ -238,18 +213,11 @@ class Ongaq {
       return this._volume * 100
     }
 
-    set previousVolume(v){
-      this._previousVolume = v
-    }
-
-    get previousVolume() {
-      return this._previousVolume
-    }
-
     set bpm(v){
       let bpm = Helper.toInt(v, { max: DEFAULTS.MAX_BPM, min: DEFAULTS.MIN_BPM })
+      if (!bpm) return false
+      this._bpm = bpm
       this.parts.forEach(p => { p.bpm = bpm })
-      if(bpm) this._bpm = bpm
     }
 
     get bpm(){
