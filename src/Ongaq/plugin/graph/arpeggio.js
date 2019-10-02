f34b8cdc75810e78b9b30ff919c064fc1501a154
Hiroyuki Takakura <takakura@codeninth.com>
2019-10-02 12:11:21 +0900

    update plugins

--- src/Ongaq/plugin/graph/arpeggio.js
+++ src/Ongaq/plugin/graph/arpeggio.js
@@ -5,36 +5,32 @@
     filter: n=>n%4
   }
 */
-const plugin = (()=>{
+const plugin = (o = {},graph = {})=>{
 
-    return (o = {},graph = {})=>{
+    let newLayer = []
 
-        let newLayer = []
+    let targetLayer = (()=>{
+        if(graph.layer.length === 0) return false
+        for(let i = graph.layer.length - 1,l = 0; i>=l; i--){
+            if(graph.layer[i][0].invoker === "audioBufferLine") return graph.layer[i]
+        }
+        return false
+    })()
+    if(!targetLayer) return false
+    let step = typeof o.step === "number" && o.step < 16 ? o.step : 1
 
-        let targetLayer = (()=>{
-            if(graph.layer.length === 0) return false
-            for(let i = graph.layer.length - 1,l = 0; i>=l; i--){
-                if(graph.layer[i][0].invoker === "audioBufferLine") return graph.layer[i]
+    targetLayer.forEach((target,i)=>{
+        newLayer.push({
+            invoker: "delayLine",
+            data: {
+                delayTime: (graph._secondsPerBeat * (i <= 3 ? i : 3) * step),
+                targetIndex: i
             }
-            return false
-        })()
-        if(!targetLayer) return false
-        let step = typeof o.step === "number" && o.step < 16 ? o.step : 1
-
-        targetLayer.forEach((target,i)=>{
-            newLayer.push({
-                invoker: "delayLine",
-                data: {
-                    delayTime: (graph._secondsPerBeat * (i <= 3 ? i : 3) * step),
-                    targetIndex: i
-                }
-            })
         })
+    })
 
-        return newLayer
+    return newLayer
 
-    }
-
-}).call(undefined,window || {})
+}
 
 export default plugin
--- src/Ongaq/plugin/graph/phrase.js
+++ src/Ongaq/plugin/graph/phrase.js
@@ -14,51 +14,45 @@ import Helper from "../../module/Helper"
   layer of path:
   [ name_of_key, length, volume ]
 */
-const plugin = (()=>{
+const plugin = (o = {},graph = {})=>{
 
-    return (o = {},graph = {})=>{
+    if(!Array.isArray(o.path) || o.path.length === 0) return false
 
-        if(!Array.isArray(o.path) || o.path.length === 0) return false
+    let distance = 0 // pathの開始から何拍目に移動したか
+    let newLayer = []
+    let _key, _length // 一時的な値を格納
 
-        let distance = 0 // pathの開始から何拍目に移動したか
-        let newLayer = []
-        let _key, _length // 一時的な値を格納
+    o.path.forEach(pair=>{
 
-        o.path.forEach(pair=>{
+        if(pair.length < 2) return false
+        if(pair[1] > 0) distance += pair[1]
 
-            if(pair.length < 2) return false
-            if(pair[1] > 0) distance += pair[1]
+        /*
+            get key,length as same as "note" plugin
+        */
+        _key = Helper.toKeyList(pair[0], graph.beatIndex, graph.measure )
+        _length = Helper.toLength(pair[1], graph.beatIndex, graph.measure )
+        if(!_key || !_length) return false
 
-            /*
-                get key,length as same as "note" plugin
-            */
-            _key = Helper.toKeyList(pair[0], graph.beatIndex, graph.measure )
-            _length = Helper.toLength(pair[1], graph.beatIndex, graph.measure )
-            if(!_key || !_length) return false
-
-            _key.forEach(k=>{
-                newLayer.push({
-                    invoker: "audioBufferLine",
-                    data: {
-                        buffer: {
-                            sound: graph.sound,
-                            length: pair[1] * graph._secondsPerBeat,
-                            key: k,
-                            startTime: graph.beatTime + distance * graph._secondsPerBeat
-                        },
-                        volume: pair[2] >= 0 && pair[2] <= 1 ? pair[2] : ( o.volume >= 0 && o.volume <= 100 ? o.volume / 100 : null),
-                        _secondsPerBeat: graph._secondsPerBeat
-                    }
-                })
+        _key.forEach(k=>{
+            newLayer.push({
+                invoker: "audioBufferLine",
+                data: {
+                    buffer: {
+                        sound: graph.sound,
+                        length: pair[1] * graph._secondsPerBeat,
+                        key: k,
+                        startTime: graph.beatTime + distance * graph._secondsPerBeat
+                    },
+                    volume: pair[2] >= 0 && pair[2] <= 1 ? pair[2] : ( o.volume >= 0 && o.volume <= 100 ? o.volume / 100 : null)
+                }
             })
-
-
         })
 
-        return newLayer
+    })
 
-    }
+    return newLayer
 
-}).call(undefined,window || {})
+}
 
 export default plugin
