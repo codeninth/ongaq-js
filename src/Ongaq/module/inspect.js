9bb7a39310e8bddbce9ea3145bb8fdadeca3a091
Hiroyuki Takakura <takakura@codeninth.com>
2019-10-06 23:42:02 +0900

    lint_fix

--- src/Ongaq/module/inspect.js
+++ src/Ongaq/module/inspect.js
@@ -1,26 +1,26 @@
 const inspect = ( object, policy = {}, redo = true )=>{
     let result
     switch(typeof object){
-        case "string":
-            return policy.string( object )
-        case "object":
-            if (Array.isArray(object)) return policy.array( object )
-            else return policy.object(object)
-        case "number":
-            return policy.number( object )
-        case "boolean":
-            return policy.boolean( object )
-        case "function":
-            result = object( ...policy._arguments )
-            if(typeof policy._next === "function") result = policy._next(result)
-            return redo ? inspect( result, policy, false ) : result
-        default:
-            if(policy.default){
-                if(typeof policy.default === "function") return policy.default( policy._arguments )
-                else return policy.default
-            } else {
-                return object
-            }
+    case "string":
+        return policy.string( object )
+    case "object":
+        if (Array.isArray(object)) return policy.array( object )
+        else return policy.object(object)
+    case "number":
+        return policy.number( object )
+    case "boolean":
+        return policy.boolean( object )
+    case "function":
+        result = object( ...policy._arguments )
+        if(typeof policy._next === "function") result = policy._next(result)
+        return redo ? inspect( result, policy, false ) : result
+    default:
+        if(policy.default){
+            if(typeof policy.default === "function") return policy.default( policy._arguments )
+            else return policy.default
+        } else {
+            return object
+        }
     }
 }
 
