41d25047fb402924299e80fd199b632e55382b29
Hiroyuki Takakura <takakura@codeninth.com>
2020-03-16 11:29:58 +0900

    fixed panner when offline

--- src/Ongaq/module/make/makePanner.js
+++ src/Ongaq/module/make/makePanner.js
@@ -1,41 +1,13 @@
-const v = {
-    w: window.innerWidth,
-    h: window.innerHeight,
-}
+import AudioCore from "../AudioCore"
 
-const _setListener = ctx => {
-    const l = ctx.listener
-
-    if (l.forwardX) {
-        l.forwardX.setValueAtTime(0, ctx.currentTime)
-        l.forwardY.setValueAtTime(0, ctx.currentTime)
-        l.forwardZ.setValueAtTime(-1, ctx.currentTime)
-        l.upX.setValueAtTime(0, ctx.currentTime)
-        l.upY.setValueAtTime(1, ctx.currentTime)
-        l.upZ.setValueAtTime(0, ctx.currentTime)
-    } else {
-        l.setOrientation(0, 0, -1, 0, 1, 0)
-    }
-
-    if (l.positionX) {
-        l.positionX.value = v.w / 2
-        l.positionY.value = v.h / 2
-        l.positionZ.value = 300
-    } else {
-        l.setPosition(v.w / 2, v.h / 2, 300)
-    }
-    l.__initByOngaq = true
-}
-
-const makePanner = ({ x }, ctx)=>{
-    if(!ctx.listener.__initByOngaq) _setListener( ctx )
+const makePanner = ({ x }, ctx) => {
     const p = ctx.createPanner()
     p.refDistance = 1000
     p.maxDistance = 10000
     p.coneOuterGain = 1
 
     const _o = [1, 0, 0]
-    if(p.orientationX) {
+    if (p.orientationX) {
         p.orientationX.setValueAtTime(_o[0], ctx.currentTime)
         p.orientationY.setValueAtTime(_o[1], ctx.currentTime)
         p.orientationZ.setValueAtTime(_o[2], ctx.currentTime)
@@ -43,11 +15,11 @@ const makePanner = ({ x }, ctx)=>{
         p.setOrientation(..._o)
     }
 
-    const xValue = ((_x)=>(typeof _x === "number" && _x >= -90 && _x <= 90) ? _x : 0)( x )
-    // mastering in case of width: 1000px -> multiply ratio (1000/v.w)
-    const _p = [v.w / 2 + (1000 / v.w) * v.w / 90 * xValue / 52, v.h / 2, 299 ]
+    const xValue = ((_x) => (typeof _x === "number" && _x >= -90 && _x <= 90) ? _x : 0)(x)
+    // mastering in case of width: 1000px -> multiply ratio (1000/AudioCore.spaceWidth)
+    const _p = [AudioCore.spaceWidth / 2 + (1000 / AudioCore.spaceWidth) * AudioCore.spaceWidth / 90 * xValue / 52, AudioCore.spaceHeight / 2, 299]
 
-    if(p.positionX){
+    if (p.positionX) {
         p.positionX.setValueAtTime(_p[0], ctx.currentTime)
         p.positionY.setValueAtTime(_p[1], ctx.currentTime)
         p.positionZ.setValueAtTime(_p[2], ctx.currentTime)
