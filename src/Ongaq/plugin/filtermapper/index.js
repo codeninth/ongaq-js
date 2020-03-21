6612afa242b15655454605cc946dbccc5658d105
Hiroyuki Takakura <takakura@codeninth.com>
2020-03-22 00:00:23 +0900

    notelistは撤廃

--- src/Ongaq/plugin/filtermapper/index.js
+++ src/Ongaq/plugin/filtermapper/index.js
@@ -1,14 +1,15 @@
 // Priority: 10
-export { default as empty } from "./empty"
+export { default as empty }
+from "./empty"
 
 // Priority: 140
-export { default as note } from "./note"
-
-// Priority: 145
-export { default as notelist } from "./notelist"
+export { default as note }
+from "./note"
 
 // Priority: 240
-export { default as arpeggio } from "./arpeggio"
+export { default as arpeggio }
+from "./arpeggio"
 
 //Priority: 340
-export { default as pan } from "./pan"
+export { default as pan }
+from "./pan"
