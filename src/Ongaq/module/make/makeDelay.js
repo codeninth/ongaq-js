a40492b69b79e757d057b1f66dd4943ac02da0d2
Hiroyuki Takakura <takakura@codeninth.com>
2020-07-06 22:42:58 +0900

    to reuse delaynode

--- src/Ongaq/module/make/makeDelay.js
+++ src/Ongaq/module/make/makeDelay.js
@@ -3,17 +3,16 @@ let pool = DelayPool.pool
 let periods = DelayPool.periods
 
 const RETRIEVE_INTERVAL = 4
-const PADDING = 30
+const PADDING = 24
 
 const makeDelay = ({ delayTime, end }, ctx )=>{
 
     if(periods[periods.length-1] < end){
         pool.push([])
         periods.push( end + RETRIEVE_INTERVAL )
-
     } else if (periods[0] + PADDING < end) {
         // to free old delayNodes
-        pool[0].forEach((usedDelay) => {
+        pool[0] && pool[0].forEach((usedDelay) => {
             DelayPool.retrieve(usedDelay)
         })
         periods = periods.slice(1)
